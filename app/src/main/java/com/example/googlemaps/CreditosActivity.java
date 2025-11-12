008b6efef710ef3e4b4f9914bb84bb3b846253dc
JuanWillian <juanwcaraujo28@gmail.com>
2025-11-12 17:52:24 -0300

    Initial commit

---
+++ app/src/main/java/com/example/googlemaps/CreditosActivity.java
@@ -0,0 +1,40 @@
+package com.example.googlemaps;
+
+import android.os.Bundle;
+import android.text.Html;
+import android.text.method.LinkMovementMethod;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class CreditosActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_creditos);
+        gitHubUsername();
+    }
+    public void gitHubUsername(){
+        TextView textViewG = (TextView)findViewById(R.id.Gabriel);
+        TextView textViewJH = (TextView)findViewById(R.id.JH);
+        TextView textViewJW = (TextView)findViewById(R.id.JW);
+
+        textViewG.setClickable(true); textViewG.setMovementMethod(LinkMovementMethod.getInstance());
+        textViewJH.setClickable(true); textViewJH.setMovementMethod((LinkMovementMethod.getInstance()));
+        textViewJW.setClickable(true); textViewJW.setMovementMethod((LinkMovementMethod.getInstance()));
+
+        String textG = "<a href='https://github.com/Gab1203'>Gabriel Fraga</a>";
+        String textJH = "<a href='https://github.com/HenrykMendes'>João Henrique Mendes</a>";
+        String textJW = "<a href='https://github.com/JuanWillian'>Juan Willian</a>";
+
+        textViewG.setText(Html.fromHtml(textG));
+        textViewJH.setText(Html.fromHtml(textJH));
+        textViewJW.setText(Html.fromHtml(textJW));
+
+    }
+}
