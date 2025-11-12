b46a236e07871b86d4fe57d5262c6ac57622f6cc
HenrickMendes <h06.987654321@gmail.com>
2025-11-12 09:43:20 -0300

    feat: CreditosActivity, MainActivity, activity_creditos.xml

--- app/src/main/java/com/example/mainactivity/CreditosActivity.java
+++ app/src/main/java/com/example/mainactivity/CreditosActivity.java
@@ -1,6 +1,9 @@
 package com.example.mainactivity;
 
 import android.os.Bundle;
+import android.text.Html;
+import android.text.method.LinkMovementMethod;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,12 +16,24 @@ public class CreditosActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_creditos);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        gitHubUsername();
+    }
+    public void gitHubUsername(){
+        TextView textViewG = (TextView)findViewById(R.id.Gabriel);
+        TextView textViewJH = (TextView)findViewById(R.id.JH);
+        TextView textViewJW = (TextView)findViewById(R.id.JW);
+
+        textViewG.setClickable(true); textViewG.setMovementMethod(LinkMovementMethod.getInstance());
+        textViewJH.setClickable(true); textViewJH.setMovementMethod((LinkMovementMethod.getInstance()));
+        textViewJW.setClickable(true); textViewJW.setMovementMethod((LinkMovementMethod.getInstance()));
+
+        String textG = "<a href='https://github.com/Gab1203'>Gabriel Fraga</a>";
+        String textJH = "<a href='https://github.com/HenrykMendes'>João Henrique Mendes</a>";
+        String textJW = "<a href='https://github.com/JuanWillian'>Juan Willian</a>";
+
+        textViewG.setText(Html.fromHtml(textG));
+        textViewJH.setText(Html.fromHtml(textJH));
+        textViewJW.setText(Html.fromHtml(textJW));
+
     }
 }
