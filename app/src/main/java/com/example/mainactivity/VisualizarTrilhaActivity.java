c1353c93e39293be46892a75660da08f186f18d8
JuanWillian <juanwcaraujo28@gmail.com>
2025-11-19 17:42:05 -0300

    feat: criando activity de visualizar a trilha

--- app/src/main/java/com/example/mainactivity/VisualizarTrilhaActivity.java
+++ app/src/main/java/com/example/mainactivity/VisualizarTrilhaActivity.java
@@ -1,51 +1,100 @@
 package com.example.mainactivity;
 
-import androidx.fragment.app.FragmentActivity;
-
 import android.os.Bundle;
+import android.widget.TextView;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.mainactivity.dao.PontoTrilhaDAO;
+import com.example.mainactivity.dao.TrilhaDAO;
+import com.example.mainactivity.model.PontoTrilha;
+import com.example.mainactivity.model.Trilha;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
-import com.example.mainactivity.databinding.ActivityVizualizarBinding;
+import com.google.android.gms.maps.model.PolylineOptions;
 
-public class VisualizarActivity extends FragmentActivity implements OnMapReadyCallback {
+import java.util.List;
 
-    private GoogleMap mMap;
-    private ActivityVizualizarBinding binding;
+public class VisualizarTrilhaActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    private GoogleMap map;
+    private TrilhaDAO trilhaDAO;
+    private PontoTrilhaDAO pontoDAO;
+
+    private Trilha trilhaSelecionada;
+    private List<PontoTrilha> pontos;
+
+    private TextView tvNome, tvData, tvDistancia, tvDuracao, tvVelocidade, tvVelocidadeMax;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_visualizar_trilha);
 
-        binding = ActivityVizualizarBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
+        trilhaDAO = new TrilhaDAO(this);
+        pontoDAO = new PontoTrilhaDAO(this);
 
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
+        tvNome = findViewById(R.id.tvNomeTrilha);
+        tvData = findViewById(R.id.tvDataTrilha);
+        tvDistancia = findViewById(R.id.tvDistancia);
+        tvDuracao = findViewById(R.id.tvDuracao);
+        tvVelocidade = findViewById(R.id.tvVelocidadeMedia);
+        tvVelocidadeMax = findViewById(R.id.tvVelocidadeMaxima);
+
+        long trilhaId = getIntent().getLongExtra("trilha_id", -1);
+
+        trilhaSelecionada = trilhaDAO.buscarPorId(trilhaId);
+        pontos = pontoDAO.listarPontos(trilhaId);
+
+        preencherInfoTrilha();
+
+        SupportMapFragment mapFragment =
+                (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.mapTrilha);
+
+        if (mapFragment != null)
+            mapFragment.getMapAsync(this);
     }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
-    @Override
-    public void onMapReady(GoogleMap googleMap) {
-        mMap = googleMap;
+    private void preencherInfoTrilha() {
+        if (trilhaSelecionada == null) return;
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        tvNome.setText(trilhaSelecionada.getNome());
+
+        tvData.setText(
+                "Início: " + trilhaSelecionada.getDataInicio() +
+                        " " + trilhaSelecionada.getHoraInicio()
+        );
+
+        tvDistancia.setText("Distância: " + trilhaSelecionada.getDistanciaPercorrida() + " m");
+
+        String duracao = trilhaSelecionada.getHoraInicio() + " → " + trilhaSelecionada.getHoraFim();
+        tvDuracao.setText("Duração: " + duracao);
+
+        tvVelocidade.setText("Velocidade média: " + trilhaSelecionada.getVelocidadeMedia() + " km/h");
+
+        tvVelocidadeMax.setText("Velocidade máxima: " + trilhaSelecionada.getVelocidadeMaxima() + " km/h");
+    }
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+        this.map = googleMap;
+
+        if (pontos == null || pontos.isEmpty()) return;
+
+        PolylineOptions poly = new PolylineOptions();
+
+        for (PontoTrilha p : pontos) {
+            LatLng pos = new LatLng(p.getLatitude(), p.getLongitude());
+            poly.add(pos);
+        }
+
+        map.addPolyline(poly);
+
+        LatLng inicio = new LatLng(pontos.get(0).getLatitude(), pontos.get(0).getLongitude());
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(inicio, 17f));
     }
 }
