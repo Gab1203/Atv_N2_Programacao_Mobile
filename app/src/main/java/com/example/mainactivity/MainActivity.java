431bf9d5a05951abce43fb1a91ee53208574a722
GabrielFraga <gabrielbff2006@gmail.com>
2025-11-19 18:38:37 -0300

    fix: botão de visualizar trilha funcional

--- app/src/main/java/com/example/mainactivity/MainActivity.java
+++ app/src/main/java/com/example/mainactivity/MainActivity.java
@@ -43,6 +43,13 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(new Intent(getApplicationContext(), ConfiguracaoActivity.class));
             }
         });
+
+        visualizarTrilhaBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), VisualizarTrilhaActivity.class));
+            }
+        });
     }
 
 
