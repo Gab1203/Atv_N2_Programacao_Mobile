fec377d1dabdf7bdabb8b0ff05cf997262317212
J. Henrique Mendes <h06.987654321@gmail.com>
2025-12-02 00:01:37 -0300

    feat: insersão de comentários em DataUtil

--- app/src/main/java/com/example/mainactivity/DataUtil.java
+++ app/src/main/java/com/example/mainactivity/DataUtil.java
@@ -5,15 +5,19 @@ import java.util.Date;
 
 public class DataUtil {
 
-    public static String getDataAtual() {
+    public static String getDataAtual() { // Faz return com data atual no formato padrão de "dd/MM/yyyy"
         return new SimpleDateFormat("dd/MM/yyyy").format(new Date());
     }
 
-    public static String getHoraAtual() {
+    public static String getHoraAtual() { // Faz return com hora atual formatada no padrão "HH:mm:ss"
         return new SimpleDateFormat("HH:mm:ss").format(new Date());
     }
 
     public static String getDataHoraAtual() {
+        /**
+         * data e hora atuais formatadas no padrão dd/MM/yyyy HH:mm:ss para
+         * regustrar data e hora do começo e fim das trilhas
+         */
         return new SimpleDateFormat("dd/MM/yyyy HH:mm:ss").format(new Date());
     }
 }
